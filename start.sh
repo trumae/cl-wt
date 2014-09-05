f16202f6988343f15d817c931e7f37aca0d2e592
Trunmae <trumae@gmail.com>
2014-09-05 11:19:42 -0500

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-#LISP="/home/trumae/lisp/ccl/lx86cl64 -K utf-8"
-LISP="sbcl"
+LISP="/home/trumae/lisp/ccl/lx86cl64 -K utf-8"
+#LISP="sbcl"
 #LISP="/home/trumae/lisp/abcl/abcl-src-1.3.1/abcl" # not supported. problems with md5 library
 
 ${LISP} <<EOF
@@ -9,7 +9,7 @@ ${LISP} <<EOF
 (ql:quickload :cl-wt)
 (in-package :cl-wt)
 (start-helloworld 8000)
-(cl:sleep 1000000)
+(loop (cl:sleep 1))
 EOF
 
 
